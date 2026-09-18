226a78bbeeae309ef386afdcc0280a8ae77cc15b
Nemondor <124510043+OisinV@users.noreply.github.com>
2026-09-18 09:29:34 +0200

    preferred readable

---
+++ prac1/tumblehouse-pr.py
@@ -0,0 +1,12 @@
+from turtle import * # type:ignore
+
+def house():
+    for i in range(7):
+        if i<5: left(90)
+        else: left(45 if i == 5 else 90)
+        forward(50 if i<5 else 35)
+
+for x, y in [(-100, 0), (35, -35), (150, -50)]:
+    teleport(x, y)
+    house()
+    left(90)
